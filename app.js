9eb877cd0e7f303a21c6a8ba05bec17e41f61737
Elnar <116576971+ZulfuqarovElnar@users.noreply.github.com>
2023-08-02 12:40:55 +0400

    Api added (#5)

--- app.js
+++ app.js
@@ -38,14 +38,113 @@ document.addEventListener("DOMContentLoaded", function () {
 // * FactDesription clicked
 const factDescriptions = document.querySelectorAll(".factDesription");
 
-    factDescriptions.forEach((fact) => {
-        const factPlusIcon = fact.querySelector(".fact-plus i");
-        const factClickElement = fact.nextElementSibling; 
+let loadingFlag = false; 
 
-        fact.addEventListener("click", () => {
-            const isOpen = factPlusIcon.classList.contains("fa-minus");
-            factPlusIcon.classList.toggle("fa-plus", isOpen);
-            factPlusIcon.classList.toggle("fa-minus", !isOpen);
-            factClickElement.style.display = isOpen ? "none" : "block";
-        });
+factDescriptions.forEach((fact) => {
+  const factPlusIcon = fact.querySelector(".fact-plus i");
+  const factClickElement = fact.nextElementSibling;
+
+  fact.addEventListener("click", async () => {
+    if (!loadingFlag) {
+      loadingFlag = true; 
+
+      const isOpen = factPlusIcon.classList.contains("fa-minus");
+      factPlusIcon.classList.toggle("fa-plus", isOpen);
+      factPlusIcon.classList.toggle("fa-minus", !isOpen);
+      factClickElement.style.display = isOpen ? "none" : "block";
+
+      try {
+        const factIndex = Array.from(factDescriptions).indexOf(fact);
+        await catImages(factIndex + 1);
+      } catch (error) {
+        console.error(error);
+      }
+      loadingFlag = false; 
+    }
+  });
+});
+
+// * Cat Api added
+
+const accessKey = "zznK8bJyCOG783FzSPbnL9lC0E3b0sCw2y6XxkXQ1g0";
+
+const factItemEls = document.querySelectorAll('.factItem');
+let page = 1;
+let currentPageResults = [];
+let apiCallInProgress = false;
+let previouslyDisplayedIndexes = [];
+let loadingFlags = [];
+
+async function catImages(factIndex) {
+  inputData = "cat";
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  const factClickElement = document.getElementById(`factClick${factIndex}`);
+
+  factClickElement.innerHTML = "Loading...";
+  factClickElement.classList.remove("error");
+  factClickElement.classList.add("loading");
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (page === 1) {
+      factClickElement.innerHTML = "";
+    }
+
+    const currentPageResults = data.results.filter((result, index) => {
+      return !previouslyDisplayedIndexes.includes(index);
     });
+
+    if (currentPageResults.length === 0) {
+      page++;
+      previouslyDisplayedIndexes = [];
+      return catImages(factIndex);
+    }
+
+    const randomIndex = Math.floor(Math.random() * currentPageResults.length);
+    const result = currentPageResults[randomIndex];
+    const resultIndexInData = data.results.indexOf(result);
+    previouslyDisplayedIndexes.push(resultIndexInData);
+
+    const factCardHTML = `
+      <div class="fact-card">
+        <img src="${result.urls.small}" alt="${result.alt_description}">
+        <a href="${result.links.html}" target="_blank" rel="noopener noreferrer">
+          ${result.alt_description}
+        </a>
+      </div>
+    `;
+
+    factClickElement.innerHTML = factCardHTML;
+    factClickElement.classList.remove("loading"); 
+
+    apiCallInProgress = false;
+  } catch (error) {
+    factClickElement.innerHTML = "Error occurred while fetching data.";
+    factClickElement.classList.add("error");
+    factClickElement.classList.remove("loading");
+
+    apiCallInProgress = false;
+  }
+}
+
+factItemEls.forEach((factItemEl, index) => {
+  const factDescriptionEl = factItemEl.querySelector('.factDesription');
+
+  loadingFlags[index] = false;
+
+  factDescriptionEl.addEventListener('click', () => {
+    if (!loadingFlags[index]) {
+      loadingFlags[index] = true;
+      catImages(index + 1)
+        .then(() => {
+          loadingFlags[index] = false;
+        })
+        .catch((error) => {
+          loadingFlags[index] = false;
+          console.error(error);
+        });
+    }
+  });
+});
